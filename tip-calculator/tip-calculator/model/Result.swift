88c928c36b943d70a769829911c95dedcd4e27d4
Kim Ruan Lopes <kim.ruan@prax.net.br>
2026-06-16 08:05:40 -0300

    Add view model and define the inputs and outputs

---
+++ tip-calculator/tip-calculator/model/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  tip-calculator
+//
+//  Created by Kim Lopes on 16/06/26.
+//
+
+import Foundation
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTip: Double
+}
---
+++ tip-calculator/tip-calculator/view model/CalculatorVM.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorVM.swift
+//  tip-calculator
+//
+//  Created by Kim Lopes on 16/06/26.
+//
+
+import Combine
+
+class CalculatorVM {
+    
+    struct Input {
+        let billPublisher: AnyPublisher<Double, Never>
+        let tipPublisher: AnyPublisher<Tip, Never>
+        let splitPublisher: AnyPublisher<Int, Never>
+    }
+    
+    struct Output {
+        let updateViewPublisher: AnyPublisher<Result, Never>
+    }
+    
+    func transform(input: Input) -> Output {
+        let result = Result(
+            amountPerPerson: 500.0,
+            totalBill: 1000.0,
+            totalTip: 50.0
+        )
+        
+        return Output(updateViewPublisher: Just(result).eraseToAnyPublisher())
+    }
+}
