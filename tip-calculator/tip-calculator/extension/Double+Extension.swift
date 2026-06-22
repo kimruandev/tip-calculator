021954b2d6dc5106e74d6c33d7d519d625be5183
Kim Ruan Lopes <kim.ruan@prax.net.br>
2026-06-21 22:46:00 -0300

    Format currency values

---
+++ tip-calculator/tip-calculator/extension/Double+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Double+Extension.swift
+//  tip-calculator
+//
+//  Created by Kim Lopes on 21/06/26.
+//
+
+import Foundation
+
+extension Double {
+    var currencyFormatted: String {
+        var isWholeNumber: Bool {
+            isZero ? true: !isNormal ? false: self == rounded()
+        }
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = isWholeNumber ? 0 : 2
+        return formatter.string(for: self) ?? ""
+    }
+}
